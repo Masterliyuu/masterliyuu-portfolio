db184c12c995b6cf4ad6132aef75448513985fe1
Leo Lingel Pañares <panaresleolingel@gmail.com>
2025-08-20 16:19:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-// script.js
 document.querySelector("form").addEventListener("submit", function (e) {
   e.preventDefault();
-  alert("Thanks for reaching out, Leo will get back to you soon!");
+  alert("Thanks for reaching out! Leo will get back to you soon.");
+  this.reset();
+});
+
+document.getElementById("darkToggle").addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
 });
