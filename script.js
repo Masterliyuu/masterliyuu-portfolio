b05e4cdabfaae7225396d0591760bdd17b793207
Leo Lingel Pañares <panaresleolingel@gmail.com>
2025-08-20 16:12:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("contactForm");
-
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Thanks for reaching out! Leo will get back to you soon.");
-    form.reset();
-  });
+// script.js
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thanks for reaching out, Leo will get back to you soon!");
 });
